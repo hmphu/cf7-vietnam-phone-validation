d188c404c77f2fd8d27bb930f75d5f1dd417a2fc
Phu Hoang <hmp@wearefram.com>
2019-11-14 23:31:22 +0700

    Init project

---
+++ README.md
@@ -0,0 +1,14 @@
+# CF7 Vietnam Phone Validation
+> WordPress Plugin
+
+Added vietnam phone validation for CF7 tel field
+
+**This plugin allow validate vietnam phone number of these carriers**
+
+| CARRIER NAME  | PHONE NUMBER FORMAT |
+| ------------- | ------------- |
+| MobiFone  | 0120xxxxxxx <br> 0121xxxxxxx <br> 0122xxxxxxx <br> 0126xxxxxxx <br> 0128xxxxxxx <br> 070xxxxxxx <br> 076xxxxxxx <br> 077xxxxxxx <br> 078xxxxxxx <br> 079xxxxxxx <br> 089xxxxxxx <br> 090xxxxxxx <br> 093xxxxxxx  |
+| Viettel  | 0162xxxxxxx<br>0163xxxxxxx<br>0164xxxxxxx<br>0165xxxxxxx<br>0166xxxxxxx<br>0167xxxxxxx<br>0168xxxxxxx<br>0169xxxxxxx<br>032xxxxxxx<br>033xxxxxxx<br>034xxxxxxx<br>035xxxxxxx<br>036xxxxxxx<br>037xxxxxxx<br>038xxxxxxx<br>039xxxxxxx<br>086xxxxxxx<br>096xxxxxxx<br>097xxxxxxx<br>098xxxxxxx  |
+| Vinaphone | 0123xxxxxxx<br>0124xxxxxxx<br>0125xxxxxxx<br>0127xxxxxxx<br>0129xxxxxxx<br>081xxxxxxx<br>082xxxxxxx<br>083xxxxxxx<br>084xxxxxxx<br>085xxxxxxx<br>088xxxxxxx<br>091xxxxxxx<br>094xxxxxxx |
+| Vietnamobile | 0186xxxxxxx<br>0188xxxxxxx<br>056xxxxxx<br>058xxxxxx<br>092xxxxxx |
+| Gmobile | 0199xxxxxxx<br>059xxxxxx<br>099xxxxxx|
---
+++ cf7-vietnam-phone-validation.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Plugin Name: CF7 Vietnam Phone Validation
+ * Plugin URI: http://hmphu.com/
+ * Description: Added vietnam phone validation for CF7 tel field
+ * Version: 1.0.0
+ * Text Domain: cf7-vn
+ * Author: Jimmy Hoang
+ * Author URI: http://hmphu.com/
+ */
+
+class JH_CF7_Vietnam_Phone_Validation {
+	
+	const TEXT_DOMAIN = 'cf7-vn';
+
+	public function __construct() {
+		
+	}
+
+	public function run() {
+		add_filter('wpcf7_validate_tel', [$this, 'validate'], 10, 2); // Normal field
+		add_filter('wpcf7_validate_tel*',  [$this, 'validate'], 10, 2); // Req. field
+	}
+
+	public function validate($result, $tag){
+		$tagName = $tag->name;
+		$value = isset($_POST[$tagName]) ? $_POST[$tagName] : false;
+
+		if(!empty($value) && !$this->validatePhoneNumber($value)){
+			$result->invalidate( $tag, __('The telephone number is invalid.', self::TEXT_DOMAIN));
+		}
+
+		return $result;
+	}
+
+	private function validatePhoneNumber($phoneNumber) {
+		$phoneNumber = str_replace(array('-', '.', ' '), '', $phoneNumber);
+
+		// return false if number is not mobile number 
+		if (!preg_match('/^(01[2689]|03|05|07|08|09)[0-9]{8}$/', $phoneNumber)) {
+			return false;
+		}
+
+		return true;
+	}
+}
+
+$oCF7VnPhoneValidation = new JH_CF7_Vietnam_Phone_Validation();
+$oCF7VnPhoneValidation->run();
